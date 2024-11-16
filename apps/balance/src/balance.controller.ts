ccc72ba68d09b8f7c1b3fa5f22250a3fd1407fcb
TaeRaeKim <dkfhdksp@gmail.com>
2024-11-16 19:56:18 +0900

    시그니처

--- apps/balance/src/balance.controller.ts
+++ apps/balance/src/balance.controller.ts
@@ -13,7 +13,7 @@ export class BalanceController {
 
   @Post('be')
   getBe(): string {
-    Logger.log('Hello  11!');
+    Logger.log('Hello  112!');
     return this.balanceService.getHello();
   }
 }
